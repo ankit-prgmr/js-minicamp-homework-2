963c4b93dbacb97854768b41661c6410198800b2
ankit <ankitagarwal912011@gmail.com>
2017-03-15 22:08:13 +0530

    Homework-2

--- exercises.js
+++ exercises.js
@@ -3,6 +3,12 @@
 function getBiggest(x, y) {
   //x and y are integers.  Return the larger integer
   //if they are the same return either one
+  if(x>y){               // return x if it's bigger
+    return x;
+  }
+  else{                     //return y if something else
+    return y;
+  }
 }
 
 function greeting(language) {
@@ -11,15 +17,36 @@ function greeting(language) {
   //language: 'English' -> 'Hello!'
   //language: 'Spanish' -> 'Hola!'
   //if language is undefined return 'Hello!'
+  if(language === 'German'){
+    return 'Guten Tag!';
+  }
+  else if(language === 'Spanish'){
+    return 'Hola!';
+  }
+  else{                                         // Includes the case for english or any other language or if language is undefined
+    return 'Hello!';
+  }
 }
 
 function isTenOrFive(num) {
   //return true if num is 10 or 5
   //otherwise return false
+  if(num === 10 || num === 5){    // using OR operator as it's either 10 or 5
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function isInRange(num) {
   //return true if num is less than 50 and greater than 20
+  if(num <50 && num >20){           // using AND operator as num has to be both less than 50 and greater than 20
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function isInteger(num) {
@@ -29,6 +56,12 @@ function isInteger(num) {
   //-10 -> true
   //otherwise return false
   //hint: you can solve this using Math.floor
+  if(Math.floor(num) === num){    // Math.floor of a num will be equal to num if it's an integer
+    return true;
+  }
+  else{
+    return false;           // Math.floor of a num will be less than num if it's not a num
+  }
 }
 
 function fizzBuzz(num) {
@@ -36,6 +69,18 @@ function fizzBuzz(num) {
   //if num is divisible by 5 return 'buzz'
   //if num is divisible by 3 & 5 return 'fizzbuzz'
   //otherwise return num
+  if(num % 3 === 0 && num % 5 === 0){         // checks if num is divisible by 3 and 5 both and it must be the first conition to be checked
+    return 'fizzbuzz';
+  }
+  else if(num % 3 === 0){
+    return 'fizz';
+  }
+  else if(num % 5 === 0){
+    return 'buzz';
+  }
+  else{
+    return num;
+  }
 }
 
 function isPrime(num) {
@@ -44,35 +89,64 @@ function isPrime(num) {
   //hint: a prime number is only evenly divisible by itself and 1
   //hint2: you can solve this using a for loop
   //note: 0 and 1 are NOT considered prime numbers
-}
+    if(num === 0 || num === 1){               // return if number is 0 or 1
+      return false;
+    }
+    else{                                                     // go ahead if number > 1
+      var count  = 0;
+      for(var i=2 ; i< num/2 ; i++){          // A number is only divisible by 1, itself and numbers in between 2 and (number/2)
+        if(num % i === 0) {
+          count += 1;                                 // If we get any number which divides our num within our range then num is not prime
+        }
+      }
+      if(count >= 1){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+  }
+
 
 function returnFirst(arr) {
   //return the first item from the array
+  return arr[0];
 }
 
 function returnLast(arr) {
   //return the last item of the array
+  return arr[arr.length - 1];
 }
 
 function getArrayLength(arr) {
   //return the length of the array
+  return arr.length;
 }
 
 function incrementByOne(arr) {
-  //arr is an array of integers  
+  //arr is an array of integers
   //increase each integer by one
   //return the array
+  for(var i=0 ; i< arr.length ; i++){     // for loop to increment each element by 1
+    arr[i] = arr[i] + 1;
+  }
+  return arr;
 }
 
 function addItemToArray(arr, item) {
   //add the item to the end of the array
   //return the array
+  arr.push(item);
+  return arr;
 }
 
 function addItemToFront(arr, item) {
   //add the item to the front of the array
   //return the array
   //hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 }
 
 function wordsToSentence(words) {
@@ -80,26 +154,58 @@ function wordsToSentence(words) {
   //return a string that is all of the words concatenated together
   //spaces need to be between each word
   //example: ['Hello', 'world!'] -> 'Hello world!'
+  var sentence = words.join(' ');                               // uses the delimitter to join the array elements to form a sentence
+  return sentence;
 }
 
 function contains(arr, item) {
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
+  var check = false;                                                  // flag to check if item is present
+  for(var i=0; i<arr.length ; i++){
+    if(item === arr[i]){
+      check = true;                                                   // make flag true if present
+    }
+  }
+  if(check === true){                                           //check the flag value and return appropriate result
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 function addNumbers(numbers) {
   //numbers is an array of integers.
   //add all of the integers and return the value
+  var sum = 0;                                                            // holds sum of all numbers
+  for(var i=0 ; i<numbers.length; i++){
+    sum += numbers[i];
+  }
+  return sum;
 }
 
 function averageTestScore(testScores) {
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
+  var sum = 0;
+  for(var i=0 ; i<testScores.length; i++){
+    sum += testScores[i];
+  }
+  var average = sum/testScores.length ;
+  return average;
 }
 
 function largestNumber(numbers) {
   //numbers is an array of integers
   //return the largest integer
+  var largest = numbers[0];                                         // Assume first element is the largest
+  for(var i=0; i<numbers.length ; i++){
+    if(largest < numbers[i]){
+      largest = numbers[i];                                            // checks with every element and if 1st element isn't largest change to appropriate element
+    }
+  }
+  return largest;
 }
 
 //Do not modify code below this line.
